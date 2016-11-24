c229391b09997dd480c356aed475264481499366
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2016-11-24 17:07:15 +0200

    Implement GalleryImageGroups. This can be used to divide a GalleryPage to multiple divisions that each contain their own set of images. This feature is optional.

---
+++ code/GalleryImageGroup.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Class GalleryImageGroup
+ * @method DataList Images()
+ * @method DataList GalleryPages()
+ *
+ * @property string $Title
+ */
+class GalleryImageGroup extends DataObject
+{
+	private static $singular_name = 'Kuvaryhmä';
+	
+	private static $plural_name = 'Kuvaryhmät';
+	
+	private static $db = array(
+		'Title' => 'Varchar(255)',
+	);
+	
+	private static $many_many = array(
+		'Images' => 'Image',
+	);
+	
+	private static $belongs_many_many = array(
+		'GalleryPages' => 'GalleryPage',
+	);
+	
+	private static $field_labels = array(
+		'Title' => 'Otsikko',
+		'Images' => 'Kuvat',
+	);
+	
+	public function getCMSFields()
+	{
+		$fields = parent::getCMSFields();
+		
+		$fields->addFieldsToTab('Root.Main', array(
+			new TextField('Title', 'Otsikko'),
+			GalleryPage::NewUploadField(),
+		));
+		
+		return $fields;
+	}
+	
+}
